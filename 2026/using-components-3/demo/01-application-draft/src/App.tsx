77e551f54464b35ae6543ec1fd2b0bbe3a9be792
Nick Romano <nromano@esri.com>
2026-03-05 17:08:40 -0800

    step-04: map click + elevation reference-element

--- 2026/using-components-3/demo/01-application-draft/src/App.tsx
+++ 2026/using-components-3/demo/01-application-draft/src/App.tsx
@@ -1,14 +1,19 @@
 import '@arcgis/map-components/components/arcgis-map';
+import '@arcgis/map-components/components/arcgis-elevation-profile';
 import '@arcgis/map-components/components/arcgis-zoom';
 import '@esri/calcite-components/components/calcite-shell';
 import '@esri/calcite-components/components/calcite-navigation';
 import '@esri/calcite-components/components/calcite-navigation-logo';
 
 import { MorelPanel } from './components/MorelPanel';
+import { useResultsActions } from './context/ResultsContext';
 
 const mapItemId = 'ecaf67baea484e99b1b499131ae8e179';
 
 export function App(): React.JSX.Element {
+  const { handleMapClick, registerElevationProfileElement } =
+    useResultsActions();
+
   return (
     // The Shell component is used as a layout for this template
     <calcite-shell content-behind>
@@ -20,12 +25,35 @@ export function App(): React.JSX.Element {
           slot="logo"
         ></calcite-navigation-logo>
       </calcite-navigation>
-      <arcgis-map id="morel-map" item-id={mapItemId}>
+      <arcgis-map
+        id="morel-map"
+        item-id={mapItemId}
+        onarcgisViewClick={handleMapClick}
+        popup-disabled
+        ground="world-elevation"
+      >
         <div slot="top-left">top-left slot</div>
         <div slot="top-right">
           <MorelPanel />
         </div>
         <arcgis-zoom slot="bottom-right" />
+
+        {/* Hidden elevation-profile component used for elevation sampling on click. */}
+        <arcgis-elevation-profile
+          className="elevation-profile-hidden"
+          referenceElement="morel-map"
+          distanceUnit="imperial"
+          elevationUnit="imperial"
+          slot="bottom-right"
+          hideChart
+          hideLegend
+          hideSettingsButton
+          hideSelectButton
+          hideStartButton
+          hideClearButton
+          hideVisualization
+          ref={registerElevationProfileElement}
+        ></arcgis-elevation-profile>
       </arcgis-map>
     </calcite-shell>
   );
